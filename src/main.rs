87433fd4fc9a247d0c546cd44ed29108f4959298
Vaidee <vaideeswaran.c23@gmail.com>
2024-01-05 19:53:25 +0530

    Adding struct

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,24 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+
+    let vaidee = Vaidee {
+        age: 27,
+        height: 180,
+        email: String::from("vaideeswaran23@gmail.com"),
+    };
+
+    println!("{vaidee}")
 }
 
 fn calculate_length(s: &String) -> usize {
     let length = s.len(); // len() returns the length of a String
 
     length
+}
+struct Vaidee {
+    age: i32,
+    height: i32,
+    email: String,
 }
